19e4169dbedf5c14c1f601102d22e6fac5ecb09b
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2021-06-07 23:23:35 +0100

    mvp heavy wip

--- app/components/home_component.rb
+++ app/components/home_component.rb
@@ -3,22 +3,45 @@
 class HomeComponent < ApplicationComponent
   def initialize(user_counts:)
     @user_counts = user_counts
-    # TODO: get counts users => {:now => ..., :last_24h => ..., :all_time => ...}
   end
 
   def c_render
-    c_div(:class => "bg-red-500", :data => { :some => "attribute" }) do
-      c "wow"
-      c "ENV: #{Rails.env}"
-      c_div do
-        "testy 123"
-      end
-      c TestyComponent.new
-      c TestyComponent.new
-      c [TestyComponent.new, TestyComponent.new]
-      c_span do
-        "testy 456"
-      end
+    c_div(:class => "pt-2 bg-blue-500")
+    banner
+    player_counts(@user_counts)
+    features
+    # c_div(:class => "bg-red-500", :data => { :some => "attribute" }) do
+    #   c "wow"
+    #   c "ENV: #{Rails.env}"
+    #   c_div do
+    #     "testy 123"
+    #   end
+    #   c TestyComponent.new
+    #   c TestyComponent.new
+    #   c [TestyComponent.new, TestyComponent.new]
+    #   c_span do
+    #     "testy 456"
+    #   end
+    # end
+  end
+
+  private
+
+  def banner
+    c_div(:class => "ds-container") do
+      "banner wip"
+    end
+  end
+
+  def player_counts(user_counts)
+    c_div(:class => "ds-container") do
+      c(user_counts.to_s)
+    end
+  end
+
+  def features
+    c_div(:class => "ds-container") do
+      "features wip"
     end
   end
 end
---
+++ app/components/navbar_component.rb
@@ -0,0 +1,26 @@
+class NavbarComponent < ApplicationComponent
+  def c_render
+    c_div(:class => "bg-red-500 fixed top-0 w-screen") do
+      c_div(:class => "ds-global-container") do
+        home_icon
+        # outlier_studio
+        # # youtube # TODO: once you have at least 1 video
+        # twitter
+        # discord
+        # luminaries
+        # profile => {logged_out => google_login, logged_in => (view profile, settings, logout)}
+      end
+    end
+  end
+
+  private
+
+  def home_icon
+    c link_to("deep space", "/", :class => "wip_TODO")
+  end
+end
+
+# TODO: finish out the navbar to get idea for it's size.
+# maybe go with non fixed header? most action will happen down below..
+
+# all the chat will probs happen on the sticky footer on the bottom as well..
